ac03e2812353345be7b3318b57b9be84d0e16d56
caopeng <caopeng@youzan.com>
2021-01-02 11:43:38 +0800

    add:gunsFilm

--- guns-film/src/main/java/com/stylefeng/guns/rest/GunsRestServletInitializer.java
+++ guns-film/src/main/java/com/stylefeng/guns/rest/GunsRestServletInitializer.java
@@ -13,7 +13,7 @@ public class GunsRestServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(GetWayApplication.class);
+        return builder.sources(GunsRestApplication.class);
     }
 
 }
